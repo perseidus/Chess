54d7c3fbba56891b10f312d843160336e891db48
Tim Seiler <tim.seiler@students.uni-mannheim.de>
2024-08-28 23:48:32 +0200

    #1 implemented move generation for all pieces except pawns

--- src/main/java/chess/game/MoveGenerator.java
+++ src/main/java/chess/game/MoveGenerator.java
@@ -10,49 +10,15 @@ public class MoveGenerator {
 
   private static int[] kingPos;
   private static int[] tempKingPos;
-  private static boolean kingInCheck;
+  private static boolean[][] enemyAttackedSquares;
   private static String colorToTurn;
-
-  public static Piece[][] setUpBoard() {
-    Piece[][] pieces = new Piece[8][8];
-
-    for (int i = 0; i < 8; i++) {
-      pieces[1][i] = new Piece(false, PieceType.PAWN);
-      pieces[6][i] = new Piece(true, PieceType.PAWN);
-    }
-
-    pieces[0][0] = new Piece(false, PieceType.ROOK);
-    pieces[0][7] = new Piece(false, PieceType.ROOK);
-    pieces[0][1] = new Piece(false, PieceType.KNIGHT);
-    pieces[0][6] = new Piece(false, PieceType.KNIGHT);
-    pieces[0][2] = new Piece(false, PieceType.BISHOP);
-    pieces[0][5] = new Piece(false, PieceType.BISHOP);
-    pieces[0][3] = new Piece(false, PieceType.QUEEN);
-    pieces[0][4] = new Piece(false, PieceType.KING);
-
-    pieces[7][0] = new Piece(true, PieceType.ROOK);
-    pieces[7][7] = new Piece(true, PieceType.ROOK);
-    pieces[7][1] = new Piece(true, PieceType.KNIGHT);
-    pieces[7][6] = new Piece(true, PieceType.KNIGHT);
-    pieces[7][2] = new Piece(true, PieceType.BISHOP);
-    pieces[7][5] = new Piece(true, PieceType.BISHOP);
-    pieces[7][3] = new Piece(true, PieceType.QUEEN);
-    pieces[7][4] = new Piece(true, PieceType.KING);
-
-    return pieces;
-  }
 
   public static HashMap<String, List<Move>> getPossibleMoves(Piece[][] pieces) {
     colorToTurn = GameState.getInstance().getColorToTurn();
     HashMap<String, List<Move>> allMoves = new HashMap<>();
 
-    boolean[][] enemyAttackedSquares = getEnemyAttackedSquares(pieces, true);
+    enemyAttackedSquares = getEnemyAttackedSquares(pieces, true);
     kingPos = new int[]{tempKingPos[0], tempKingPos[1]};
-    if (enemyAttackedSquares[kingPos[0]][kingPos[1]]) {
-      kingInCheck = true;
-    } else {
-      kingInCheck = false;
-    }
 
     for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
@@ -92,16 +58,19 @@ public class MoveGenerator {
         moves = getPawnMoves(pieces, i, j);
         break;
       case ROOK:
-        moves = getRookMoves(pieces, i, j);
+        moves = getOrthogonalMoves(pieces, i, j);
         break;
       case KNIGHT:
         moves = getKnightMoves(pieces, i, j);
         break;
       case BISHOP:
-        moves = getBishopMoves(pieces, i, j);
+        moves = getDiagonalMoves(pieces, i, j);
         break;
       case QUEEN:
-        moves = getQueenMoves(pieces, i, j);
+        moves = getDiagonalMoves(pieces, i, j);
+        for (Move m : getOrthogonalMoves(pieces, i, j)) {
+          moves.add(m);
+        }
         break;
       case KING:
         moves = getKingMoves(pieces, i, j);
@@ -123,6 +92,7 @@ public class MoveGenerator {
     return pieceAttacksSquare;
   }
 
+  //TODO check checks for special move types
   //removes move if it leaves own king in check
   private static List<Move> removeIllegalMoves(Piece[][] pieces, List<Move> moves) {
     Piece[][] newPieces = new Piece[8][8];
@@ -132,8 +102,12 @@ public class MoveGenerator {
       for (int i = 0; i < pieces.length; i++) {
         newPieces[i] = Arrays.copyOf(pieces[i], pieces[i].length);
       }
-      newPieces[move.getTo()[0]][move.getTo()[1]] = newPieces[move.getFrom()[0]][move.getFrom()[1]];
-      newPieces[move.getFrom()[0]][move.getFrom()[1]] = null;
+      if (move.isSpecialMove()){
+        newPieces = BoardGenerator.getBoardAfterSpecialMove(newPieces, move);
+      } else {
+        newPieces[move.getTo()[0]][move.getTo()[1]] = newPieces[move.getFrom()[0]][move.getFrom()[1]];
+        newPieces[move.getFrom()[0]][move.getFrom()[1]] = null;
+      }
       attackedSquares = getEnemyAttackedSquares(newPieces, true);
 
       if (attackedSquares[tempKingPos[0]][tempKingPos[1]]) {
@@ -144,7 +118,7 @@ public class MoveGenerator {
     return moves;
   }
 
-  //TODO
+  //TODO pawn moves
   private static List<Move> getPawnMoves(Piece[][] pieces, int i, int j) {
     List<Move> moves = new ArrayList<>();
     int[] from = {i, j};
@@ -153,23 +127,217 @@ public class MoveGenerator {
     return moves;
   }
 
-  private static List<Move> getRookMoves(Piece[][] pieces, int i, int j) {
-    return null;
+  private static List<Move> getOrthogonalMoves(Piece[][] pieces, int i, int j) {
+    List<Move> moves = new ArrayList<>();
+    int[] from = {i, j};
+    int count = 1;
+
+    while ((i - count) >= 0) {              //UP
+      if (pieces[i - count][j] == null) {   //empty square
+        moves.add(new Move(from, new int[]{i - count, j}));
+      } else if (!pieces[i - count][j].getColor().equals(colorToTurn)) {  //enemy pieces square
+        moves.add(new Move(from, new int[]{i - count, j}));
+        break;
+      } else {    //own pieces square
+        break;
+      }
+      count++;
+    }
+
+    count = 1;
+    while ((j + count) <= 7) {              //RIGHT
+      if (pieces[i][j + count] == null) {
+        moves.add(new Move(from, new int[]{i, j + count}));
+      } else if (!pieces[i][j + count].getColor().equals(colorToTurn)) {
+        moves.add(new Move(from, new int[]{i, j + count}));
+        break;
+      } else {
+        break;
+      }
+      count++;
+    }
+
+    count = 1;
+    while ((i + count) <= 7) {              //DOWN
+      if (pieces[i + count][j] == null) {
+        moves.add(new Move(from, new int[]{i + count, j}));
+      } else if (!pieces[i + count][j].getColor().equals(colorToTurn)) {
+        moves.add(new Move(from, new int[]{i + count, j}));
+        break;
+      } else {
+        break;
+      }
+      count++;
+    }
+
+    count = 1;
+    while ((j - count) >= 0) {              //LEFT
+      if (pieces[i][j - count] == null) {
+        moves.add(new Move(from, new int[]{i, j - count}));
+      } else if (!pieces[i][j - count].getColor().equals(colorToTurn)) {
+        moves.add(new Move(from, new int[]{i, j - count}));
+        break;
+      } else {
+        break;
+      }
+      count++;
+    }
+
+    return moves;
   }
 
   private static List<Move> getKnightMoves(Piece[][] pieces, int i, int j) {
-    return null;
+    List<Move> moves = new ArrayList<>();
+    int[] from = {i, j};
+
+    //counter-clockwise starting at upper left move
+    if ((((i - 2) >= 0) && ((j - 1) >= 0)) && ((pieces[i - 2][j - 1] == null) ||
+        !pieces[i - 2][j - 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i - 2, j - 1}));
+    }
+    if ((((i - 1) >= 0) && ((j - 2) >= 0)) && ((pieces[i - 1][j - 2] == null) ||
+        !pieces[i - 1][j - 2].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i - 1, j - 2}));
+    }
+    if ((((i + 1) <= 7) && ((j - 2) >= 0)) && ((pieces[i + 1][j - 2] == null) ||
+        !pieces[i + 1][j - 2].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i + 1, j - 2}));
+    }
+    if ((((i + 2) <= 7) && ((j - 1) >= 0)) && ((pieces[i + 2][j - 1] == null) ||
+        !pieces[i + 2][j - 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i + 2, j - 1}));
+    }
+    if ((((i + 2) <= 7) && ((j + 1) <= 7)) && ((pieces[i + 2][j + 1] == null) ||
+        !pieces[i + 2][j + 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i + 2, j + 1}));
+    }
+    if ((((i + 1) <= 7) && ((j + 2) <= 7)) && ((pieces[i + 1][j + 2] == null) ||
+        !pieces[i + 1][j + 2].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i + 1, j + 2}));
+    }
+    if ((((i - 1) >= 0) && ((j + 2) <= 7)) && ((pieces[i - 1][j + 2] == null) ||
+        !pieces[i - 1][j + 2].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i - 1, j + 2}));
+    }
+    if ((((i - 2) >= 0) && ((j + 1) <= 7)) && ((pieces[i - 2][j + 1] == null) ||
+        !pieces[i - 2][j + 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i - 2, j + 1}));
+    }
+
+    return moves;
   }
 
-  private static List<Move> getBishopMoves(Piece[][] pieces, int i, int j) {
-    return null;
-  }
+  private static List<Move> getDiagonalMoves(Piece[][] pieces, int i, int j) {
+    List<Move> moves = new ArrayList<>();
+    int[] from = {i, j};
+    int count = 1;
 
-  private static List<Move> getQueenMoves(Piece[][] pieces, int i, int j) {
-    return null;
+    while (((i - count) >= 0) && ((j - count) >= 0)) {          //UP-LEFT
+      if (pieces[i - count][j - count] == null) {
+        moves.add(new Move(from, new int[]{i - count, j - count}));
+      } else if (!pieces[i - count][j - count].getColor().equals(colorToTurn)) {
+        moves.add(new Move(from, new int[]{i - count, j - count}));
+        break;
+      } else {
+        break;
+      }
+      count++;
+    }
+
+    count = 1;
+    while (((i - count) >= 0) && ((j + count) <= 7)) {                      //UP-RIGHT
+      if (pieces[i - count][j + count] == null) {
+        moves.add(new Move(from, new int[]{i - count, j + count}));
+      } else if (!pieces[i - count][j + count].getColor().equals(colorToTurn)) {
+        moves.add(new Move(from, new int[]{i - count, j + count}));
+        break;
+      } else {
+        break;
+      }
+      count++;
+    }
+
+    count = 1;
+    while (((i + count) <= 7) && ((j + count) <= 7)) {                      //DOWN-RIGHT
+      if (pieces[i + count][j + count] == null) {
+        moves.add(new Move(from, new int[]{i + count, j + count}));
+      } else if (!pieces[i + count][j + count].getColor().equals(colorToTurn)) {
+        moves.add(new Move(from, new int[]{i + count, j + count}));
+        break;
+      } else {
+        break;
+      }
+      count++;
+    }
+
+    count = 1;
+    while (((i + count) <= 7) && ((j - count) >= 0)) {                      //DOWN-LEFT
+      if (pieces[i + count][j - count] == null) {
+        moves.add(new Move(from, new int[]{i + count, j - count}));
+      } else if (!pieces[i + count][j - count].getColor().equals(colorToTurn)) {
+        moves.add(new Move(from, new int[]{i + count, j - count}));
+        break;
+      } else {
+        break;
+      }
+      count++;
+    }
+
+    return moves;
   }
 
   private static List<Move> getKingMoves(Piece[][] pieces, int i, int j) {
-    return null;
+    List<Move> moves = new ArrayList<>();
+    int[] from = {i, j};
+
+    //counter-clockwise starting at top square
+    if (((i - 1) >= 0) && ((pieces[i - 1][j] == null) ||
+        !pieces[i - 1][j].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i - 1, j}));
+    }
+    if ((((i - 1) >= 0) && ((j - 1) >= 0)) && ((pieces[i - 1][j - 1] == null) ||
+        !pieces[i - 1][j - 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i - 1, j - 1}));
+    }
+    if (((j - 1) >= 7) && ((pieces[i][j - 1] == null) ||
+        !pieces[i][j - 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i, j - 1}));
+    }
+    if ((((i + 1) <= 7) && ((j - 1) >= 0)) && ((pieces[i + 1][j - 1] == null) ||
+        !pieces[i + 1][j - 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i + 1, j - 1}));
+    }
+    if (((i + 1) <= 7) && ((pieces[i + 1][j] == null) ||
+        !pieces[i + 1][j].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i + 1, j}));
+    }
+    if ((((i + 1) <= 7) && ((j + 1) <= 7)) && ((pieces[i + 1][j + 1] == null) ||
+        !pieces[i + 1][j + 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i + 1, j + 1}));
+    }
+    if (((j + 1) <= 7) && ((pieces[i][j + 1] == null) ||
+        !pieces[i][j + 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i, j + 1}));
+    }
+    if ((((i - 1) >= 0) && ((j + 1) <= 7)) && ((pieces[i - 1][j + 1] == null) ||
+        !pieces[i - 1][j + 1].getColor().equals(colorToTurn))) {
+      moves.add(new Move(from, new int[]{i - 1, j + 1}));
+    }
+
+    //castling moves, right + left
+    if (pieces[i][j].isFirstMove() && !enemyAttackedSquares[i][j]) {
+      if (pieces[7][7].isFirstMove()
+          && pieces[i][j + 1] == null && pieces[i][j + 2] == null && pieces[7][6] == null
+          && !enemyAttackedSquares[i][j + 1] && !enemyAttackedSquares[i][j + 2]) {
+        moves.add(new Move(from, new int[]{i, j + 2}, SpecialMoveType.CASTLE));
+      }
+      if (pieces[7][0].isFirstMove()
+          && pieces[i][j - 1] == null && pieces[i][j - 2] == null && pieces[7][1] == null
+          && !enemyAttackedSquares[i][j - 1] && !enemyAttackedSquares[i][j - 2]) {
+        moves.add(new Move(from, new int[]{i, j - 2}, SpecialMoveType.CASTLE));
+      }
+    }
+
+    return moves;
   }
 }
